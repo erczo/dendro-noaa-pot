fc2830565d06617f32bef67d4c47bd099492af3d
chris-wong-1 <wong.christopher.s@gmail.com>
2017-03-14 22:07:52 -0700

    Add a second parser to create a select-output.json

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ fs.readFile(__dirname + '/input.xml', 'utf8', function(err, data) {
     })
   })
 });
+
+fs.readFile(__dirname + '/select-input.xml', 'utf8', function(err, data) {
+  var xmlStringSerialized = new DOMParser().parseFromString(data, 'text/xml');
+  parseString(xmlStringSerialized, function(err, result) {
+    if(err) {
+      return console.log(err);
+    }
+    json = JSON.stringify(result);
+    fs.writeFile(__dirname + '/select-output.json', json, function(err) {
+      if(err) {
+        return console.log(err);
+      }
+    })
+  })
+});
